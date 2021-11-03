1cd1715f756a9287fb221276152e0efca7cb53d9
akash <akashbwj@gmail.com>
2021-11-04 01:10:45 +0530

    Implemented feature for showing images

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -46,7 +46,20 @@ export const Results = () => {
                 </div>
             );
         case '/images':
-            return 'SEARCH';
+            return (
+                <div className="flex flex-wrap justify-center items-center">
+                    {
+                        results?.image_results?.map(({image,link: {href,title}},index)=>(
+                            <a className="sm:p-3 p-5" href={href} key={index} target="_blank" rel="noreferrer">
+                                <img src={image?.src} alt={title} loading="lazy"/>
+                                    <p className="w-36 break-words text-sm mt-2">
+                                        {title}
+                                    </p>
+                            </a>
+                        ))
+                    }
+                </div>
+            );
         case '/news':
             return 'SEARCH';
         case '/videos':
